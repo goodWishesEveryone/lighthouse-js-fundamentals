4916e884348b1d8f31ac8697210e58f17c71a698
goodWishesEveryone <goodWishesEveryone@gmail.com>
2021-10-05 12:23:11 -0400

    Use conditionals to tell people about the weather

---
+++ lighthouse/lighthouse-js-fundamentals/conditionals.js
@@ -0,0 +1,154 @@
+/* const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+// Returns : 
+// Don't forget your umbrella!
+// Now you're ready to go outside!
+*/
+
+
+
+/*
+const raining = false;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+// Returns : 
+// Now you're ready to go outside!
+*/
+
+
+
+
+/*
+const raining = false;
+const cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+//Retuns:
+//Make sure you pick out a scarf!
+//Now you're ready to go outside!
+*/
+
+
+
+
+/*
+const raining = true;
+const cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+//Returns:
+//Don't forget your umbrella!
+//Make sure you pick out a scarf!
+//Now you're ready to go outside!
+*/
+
+
+
+//What about for it to tell us that short sleeves are OK?
+/*
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+//Short sleeves are fine.
+*/
+
+
+
+/*
+const temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+//Short sleeves won't cut it!
+//Now you're ready to go outside!
+*/
+
+
+
+
+//What does temperature have to be for the program to tell us to pick a scarf?
+/*
+const temperature = -12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+//Make sure you pick out a scarf!
+//Now you're ready to go outside!
+*/
+
+
+
+const temperature = 0;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+//Short sleeves won't cut it!
+//Now you're ready to go outside!
